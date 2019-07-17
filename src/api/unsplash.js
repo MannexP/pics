8ad8c6ba2829f9ec039cdf1d016f5beaf22e1215
Mannex Pimentel <mannex247@gmail.com>
2019-07-16 23:38:22 -0400

    axios

---
+++ src/api/unsplash.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+
+export default axios.create({
+    baseURL:'https://api.unsplash.com',
+    headers: {
+        Authorization:
+          "Client-ID e608d21abf8aec446d2e3cd9eeaf3735b3bf58e9bcc071ae42d7038f2a60099c"
+      }
+});
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import SearchBar from './SearchBar'
+import React from "react";
+import unsplash from '../api/unsplash';
+import SearchBar from "./SearchBar";
+
+class App extends React.Component {
+    state = { images: []  };
 
 
-const App = () => {
-    return <div className="ui container" style={{margin: '20px'}}>
-        <SearchBar />
-    </div>
-};
+ onSearchSubmit = async (term) => {
+    const response = await unsplash.get('/search/photos', {
+      params: { query: term },
+    });
+    this.setState({images: response.data.results})
+  }
 
+  render() {
+    return (
+      <div className="ui container" style={{ margin: "20px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
+      </div>
+    );
+  }
+}
 
 export default App;
