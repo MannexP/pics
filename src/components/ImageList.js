00dab7c9ba3fb35667d40006c1494a41145f9d35
Mannex Pimentel <mannex247@gmail.com>
2019-07-18 09:26:23 -0400

    save

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-// Here props is the object that was passed from App.js "images" ******************1
+// Here props is the object that was passed from App.js "images" ******************1imageList"
+
 const ImageList = (props) => {
     console.log(props.images)
     return <div>Image List</div>
