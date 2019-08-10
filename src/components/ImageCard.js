4e434a166dbbf675958d9c7ad11963b955301937
Mannex Pimentel <mannex247@gmail.com>
2019-08-10 18:22:53 -0400

    save

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,18 +1,24 @@
 import React from "react";
 
 class ImageCard extends React.Component {
-    // constructor(props) {
-    //     super(props);
+    constructor(props) {
+        super(props);
 
-    //     this.imageRef = React.createRef();
-    // }
+        this.banana1Ref = React.createRef();
+    }
+
+    componentDidMount() {
+        console.log(this.banana1Ref
+            )
+        console.log(this.banana1Ref.current.clientHeight);
+    }
   render() {
       const {description, urls } = this.props.banana1;
 
     return (
       <div>
         <img 
-        // ref={this.imageRef}
+        ref={this.banana1Ref}
         alt={description} 
         src={urls.regular}
         />
